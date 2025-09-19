eb1c7698f989de324fa0f4319f4494486f20238d
ss_ <sebacalvi08@gmail.com>
2025-09-19 14:27:46 -0300

    Update __init__.py

--- factories/__init__.py
+++ factories/__init__.py
@@ -1,3 +1,17 @@
-# factories/__init__.py
-"""Tektra factories package marker."""
-__all__ = []
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Módulo de factories para Tektra - Bot autónomo de creatividad digital
+"""
+
+# Esto hace que Python reconozca 'factories' como un paquete
+# permitiendo imports como: from factories.web_factory import generate_site
+
+__all__ = [
+    'web_factory',
+    'image_factory', 
+    'game_factory'
+]
+
+# Versión del módulo factories
+__version__ = "1.0.0"
